0e8bd02b414e578b79b9b82e530f39c949bf477c
tomiiide <ayotomiiide@gmail.com>
2017-02-18 23:53:09 +0100

    Initial commit

---
+++ 01 - Drum Kit/run.js
@@ -0,0 +1,23 @@
+
+function Play(e) {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+  
+  if (!audio) return;
+  audio.currentTime = 0;
+  audio.play();
+  key.classList.add("playing");
+}
+
+function removeTransition(e) {
+  if (e.propertyName !== 'transform') return;
+
+  this.classList.remove('playing');
+}
+
+window.addEventListener('keydown', Play);
+const keys = Array.from(document.querySelectorAll('.key'));
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+
+
---
+++ README.md
@@ -0,0 +1,4 @@
+# My Solutions to the [JavaScript30 Tutorial](http://javascript30.com) by Wes Bos
+
+## Check List
+* [x] ~~[01 Javascript Drum Kit](https://www.google.com)~~
